35d5aecef503e43be45c4ac32642a85d76db4fb0
NORI <123169862+GGmark2@users.noreply.github.com>
2023-11-01 18:18:06 +0000

    tank you I am done for today

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <section id='print'>
+        <p className='font-extrabold text-gray-700 text-4xl'>Print Anywhere for the price of cents</p>
+
+        <div className='flex bg-white'>
+          In three simple stepps
+        </div>
+      </section>
     </main>
   )
 }
---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='flex sticky justify-between top-0 p-5 bg-[#F57C0099] text-gray-700 font-extrabold backdrop-blur-xl shadow-md'>
+        <a href='#products'>Products</a>
+        <a href='#print'>Ship-Print</a>
+    </nav>
+)
+}
+
+export default NavBar
