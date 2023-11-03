1e22da09c9c30ee4debd1d273f13002aadf15148
Watnir <137804027+AU0758@users.noreply.github.com>
2023-11-03 16:40:35 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,30 +1,54 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [materials, setMaterials] = useState({});
+  const [printers, setPrinters] = useState({});
+
+  console.log(printers)
+
   return (
-    <main>
-      <section id='model' className='flex'>
-        <input type='file'/>
-
-        <div className='flex bg-white carrousel_y'>
-          {materials?.map((material) => {
-            <div className={`flex justify-center items-center bg-[${material.bg}] material`}>
-              <Image width={800} height={800} src={material.image} className="exebit"/>
+    <main className='flex flex-col'>
+      {materials !== {} ? 
+        <form>
+          <section id='model' className='flex w-[100%]'>
+            <input type='file' className='w-[100%] cursor-pointer'/>
+            <div className='flex bg-white carrousel_y w-[100%]'>
+              {materials?.map((material) => {
+                <div className={`flex justify-center items-center trasition-all bg-[${material.bg}] material cursor-pointer`}>
+                  <p className={`absolute right-0 top-0 m-1 text-white opacity-80`}>{material.price}</p>
+                  <Image width={800} height={800} src={material.image} className="exebit"/>
+                </div>
+              })}
             </div>
-          })}
-        </div>
-      </section>
-      <section id='printer' className='flex'>
-        <input type='file'/>
-
-        <div className='flex bg-white carrousel_y'>
-          {printers?.map((printer) => {
-            <div className={`flex justify-center items-center bg-[${printer.bg}] material`}>
-              <Image width={800} height={800} src={printer.image} className="exebit"/>
+          </section>
+          <section id='printer' className='flex w-[100%]'>
+            <div className='flex bg-white carrousel_y w-[100%]'>
+              {printers?.map((printer) => {
+                <div className={`flex justify-center items-center bg-[${printer.bg}] material cursor-pointer`}>
+                  <Image width={800} height={800} src={printer.image} className="exebit"/>
+                </div>
+              })}
             </div>
-          })}
+          </section>
+          <section id='send' className='flex justify-center items-center w-[100%] h-[800px]'>
+            <div>
+              <button className='text-white active:scale-90 cursor-pointer'>Send</button>
+              <div className='flex justify-between'>
+                {materials.price}
+              </div>
+            </div>
+          </section>
+        </form> 
+      :
+        <div className='flex flex-col justify-center max-w-[800px] p-5'>
+          <h1>ERRO!</h1>
+          <p>Não há recursos para efetuar a operação</p>
         </div>
-      </section>
+      }
+
     </main>
   )
 }
