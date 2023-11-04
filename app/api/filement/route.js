c208f9c08950148333372fd569db1233aa0ff782
AU0758 <a07582709@gmail.com>
2023-11-04 17:27:16 +0000

    V.1.4.3

--- app/api/filement/route.js
+++ app/api/filement/route.js
@@ -3,10 +3,16 @@ import { NextResponse } from "next/server";
 import connectMongoDB from "@/libs/mongodb";
 
 export async function POST(req) {
-    const {price, name, image, bg, lenght} = await req.json();
+    const { price, name, image, bg, length } = await req.json();
 
     await connectMongoDB();
-    await Filement.create({price, name, image, bg, lenght});
 
-    return NextResponse.json({ message: "Filement created successfully" }, { status: 201 });
-};
+    try {
+        const filament = await Filement.create({ price, name, image, bg, length });
+        console.log("Filament created:", filament);
+        return NextResponse.json({ message: "Filament created successfully" }, { status: 201 });
+    } catch (error) {
+        console.error("Error creating filament:", error);
+        return NextResponse.json({ message: "Failed to create filament" }, { status: 500 });
+    }
+}
