06747e44fb40ff0e61057778198b2063dd156cb6
AU0758 <a07582709@gmail.com>
2023-11-04 19:20:15 +0000

    V.1.4.4

---
+++ app/api/printer/route.js
@@ -0,0 +1,18 @@
+import Printer from "@/models/printer";
+import { NextResponse } from "next/server";
+import connectMongoDB from "@/libs/mongodb";
+
+export async function POST(req) {
+    const { price, name, image, bg, length } = await req.json();
+
+    await connectMongoDB();
+
+    try {
+        const printer = await Printer.create({ price, name, image, bg, length });
+        console.log("Priner Added:", printer);
+        return NextResponse.json({ message: "Priner Added successfully" }, { status: 201 });
+    } catch (error) {
+        console.error("Error creating Priner:", error);
+        return NextResponse.json({ message: "Failed to create Priner" }, { status: 500 });
+    }
+}
